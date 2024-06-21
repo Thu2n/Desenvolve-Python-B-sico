2ad97d17c349999f70c5bcffc7cfbc4b6dda78ad
Thu2n <thuany.silva@pditabira.com>
2024-06-20 22:36:20 -0300

    Add files via upload

---
+++ Modulo 2/aula2_questao2.py
@@ -0,0 +1,13 @@
+Texto = "o resultado é"
+Valor1 = 10
+Valor2 = 3.5
+Result = Valor1 + Valor2
+
+print(Texto)
+print(type(Texto))
+print(Valor1)
+print(type(Valor1))
+print(Valor2)
+print(type(Valor2))
+print(Result)
+print(type(Result))
